3c60061e1877983f4ec81001a097444f347031b1
srmt99 <soroushmortazavi99@gmail.com>
2024-02-24 15:27:24 -0500

    redirect after login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from . import views
+from django.contrib.auth import views as auth_views
 from .views import SignUpView
 
 
 urlpatterns = [
+    path('', views.acc_article_list, name='acc_article_list'),
     path("signup/", SignUpView.as_view(), name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from articles.models import Article
 
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
@@ -10,3 +12,9 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+@login_required
+def acc_article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'acc_article_list.html', {'articles': articles})
+
