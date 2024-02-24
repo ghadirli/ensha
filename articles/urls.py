6c4dcb8f471e28e8f54d8576d912a8fae3fb6dfc
srmt99 <soroushmortazavi99@gmail.com>
2024-02-24 10:49:37 -0500

    first commit

---
+++ articles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('create/', views.create_article, name='article_list'),
+    path('<int:article_id>/', views.article_detail, name='article_detail'),
+    path('', views.article_list, name='article_list'),
+
+]
---
+++ articles/views.py
@@ -0,0 +1,31 @@
+# In articles/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Article
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'article_list.html', {'articles': articles})
+
+
+@login_required
+def create_article(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.user
+
+        new_article = Article.objects.create(title=title, content=content, author=author)
+        return redirect('article_detail', article_id=new_article.id)
+        # Process form data and create new article
+        # Remember to associate the article with the logged-in user
+        # Redirect to article detail view or article list view
+    else:
+        return render(request, 'create_article.html')
+
+
+def article_detail(request, article_id):
+    article = Article.objects.get(pk=article_id)
+    return render(request, 'article_detail.html', {'article': article})
