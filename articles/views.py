f9cec1dde8e3b2525e0fee03b195448f7fa5f96e
Ali Ghadirli <alighadirli@v1040-wn-rt-a-44-245.campus-dynamic.uwaterloo.ca>
2024-02-24 18:07:56 -0500

    changed the create article page

--- articles/views.py
+++ articles/views.py
@@ -5,7 +5,7 @@ from .models import Article
 
 
 def article_list(request):
-    articles = Article.objects.all()
+    articles = Article.objects.order_by('-created_at')
     return render(request, 'article_list.html', {'articles': articles})
 
 
