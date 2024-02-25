fa5529887435397184bf65a84e0fd37745e26f1f
Ali Ghadirli <alighadirli@Alis-MacBook-Air.local>
2024-02-25 18:46:39 -0500

    edit button added, need to work on the appearance of the page

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('<int:article_id>/', views.article_detail, name='article_detail'),
+    path('<int:article_id>/edit', views.edit_article, name='edit_article'),
     path('', views.article_list, name='article_list'),
 
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,28 @@
 # In articles/views.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Article
+from .forms import ArticleForm
 
 
 def article_list(request):
     articles = Article.objects.order_by('-created_at')
     return render(request, 'article_list.html', {'articles': articles})
 
+
+def edit_article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    if request.user == article.author:
+        if request.method == 'POST':
+            form = ArticleForm(request.POST, instance=article)
+            if form.is_valid():
+                form.save()
+                return redirect('article_detail', article_id=article_id)
+        else:
+            form = ArticleForm(instance=article)
+        return render(request, 'edit_article.html', {'form': form})
+    else:
+        return redirect('article_detail', article_id=article_id)
 
 def show_base(request):
     return render(request, 'base.html', {})
